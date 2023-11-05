092bfb76157b3d2de12b7880de62f027053f7d99
IzharPasha <mdizharpasha07@gmail.com>
2023-11-05 16:23:46 +0530

    Just check

---
+++ script.js
@@ -0,0 +1,53 @@
+                    // SIDE NAVIGATIONI
+
+const openbtn = document.querySelector(".open-btn");
+const closebtn = document.querySelector(".close-btn");
+const wrap = document.querySelectorAll(".wrap");
+const togglebtn = document.querySelectorAll("#toggle-btn");
+
+openbtn.addEventListener("click", () => {
+    for (let x = 0; x < wrap.length; x++){
+        wrap[x].classList.add('visible');
+    }
+    
+})
+
+closebtn.addEventListener("click", () => {
+    for (let x = 0; x < wrap.length; x++){
+        wrap[x].classList.remove('visible');
+    }
+    
+})
+
+
+
+function homepge() {
+    var element = document.querySelector("#profile-pic");
+    element.classList.add("profilepic");
+
+
+    var element1 = document.querySelector("#my-info");
+    element1.classList.add("profileinfo");
+}
+
+homepge() ;
+
+
+function aboutimg() {
+    var element2 = document.querySelector("#about-container");
+    element2.classList.add("aboutimg");
+}
+
+function toggle(){
+    var toggbtn = document.body;
+    toggbtn.classList.toggle("darkmode");
+}
+
+
+function mylog() {
+    var element3 = document.querySelector("");
+    element3.classList.add("mylogo");
+
+    // var element3 = document.querySelector("#log-css");
+    // element3.classList.add("mylogo");
+}
